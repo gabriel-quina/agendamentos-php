d9c9df171cfaed841478d9bb161043adce7304d1
Gabriel Quina <gabriel.quina@ymail.com>
2023-07-01 22:56:59 -0300

    Configuração inicial composer.json e adicionado variaveis de ambiente

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+
+require __DIR__ . '\vendor\autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-Br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AGENDAMENTOS</title>
+</head>
+<body>
+
+</body>
+</html>
